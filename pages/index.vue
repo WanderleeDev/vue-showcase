5f24fc1dc92bcdfd3413a518987adc12a091479d
Wanderlee <wanderleedev@gmail.com>
2025-02-23 19:28:04 -0500

    Update CSS styles and enhance typography for improved responsiveness

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="grid place-content-center h-full">
-    <h1 class="text-[30vw] font-extrabold">V</h1>
-    <Slide class="w-80" />
+    <h1 class="title-char lg">V</h1>
+    <!-- <Slide class="w-80" /> -->
   </div>
 </template>
 <script setup lang="ts">
@@ -10,3 +10,15 @@ useSeoMeta({
   description: "My projects",
 });
 </script>
+<style scoped>
+.title-char {
+  font-size: 50vw;
+  font-weight: 900;
+  @media (width >= 768px) {
+    font-size: 40vw;
+  }
+  @media (width >= 1024px) {
+    font-size: 30vw;
+  }
+}
+</style>
